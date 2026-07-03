72135c9fa2c2035860ddbbb9c97c6db19491ad3e
akash <itzzakash@gmail.com>
2026-07-03 17:51:58 -0400

    Add nfl_teams, nfl_players_ and leagues models and migration

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from database import Base
 
 
@@ -17,3 +17,28 @@ class Season(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     season_year = Column(Integer, unique=True, nullable=False)
+
+class NFLTeam(Base):
+    __tablename__ = "nfl_teams"
+
+    id = Column(Integer, primary_key=True, index=True)
+    team_name = Column(String, unique=True, nullable=False)
+    conference = Column(String, nullable=False)
+    division = Column(String, nullable=False)
+
+
+class NFLPlayer(Base):
+    __tablename__ = "nfl_players"
+
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    team_id = Column(Integer, ForeignKey("nfl_teams.id"), nullable=False)
+
+
+class League(Base):
+    __tablename__ = "leagues"
+
+    id = Column(Integer, primary_key=True, index=True)
+    league_name = Column(String, nullable=False)
+    join_code = Column(String, unique=True, nullable=False, index=True)
